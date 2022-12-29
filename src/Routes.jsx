99371d8e8e568fc18c8dc52be276aebbcdd662a4
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2022-12-29 00:23:15 -0300

    2. Criando um formulário para identificação

---
+++ src/Routes.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Login from './pages/Login';
+import NotFound from './pages/Not-Found';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
+
+class Routes extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/search" component={ Search } />
+          <Route exact path="/album/:id" component={ Album } />
+          <Route exact path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/profile/edit" component={ ProfileEdit } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default Routes;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,67 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Loading from '../components/Loading';
+import { createUser } from '../services/userAPI';
 
 class Login extends Component {
+  state = {
+    isDisabled: true,
+    name: '',
+    isLoading: false,
+  };
+
+  handleChange = ({ target }) => {
+    const { value } = target;
+    const num = 3;
+    const valid = value.length >= num;
+    this.setState({
+      name: value,
+      isDisabled: !valid,
+    });
+  };
+
+  handleClick = async (name) => {
+    const { history: { push } } = this.props;
+    this.setState({
+      isLoading: true,
+    });
+    await createUser({ name });
+    this.setState({
+      isLoading: false,
+    });
+    return push('/search');
+  };
+
   render() {
+    const { isDisabled, name, isLoading } = this.state;
     return (
-      <div data-testid="page-login" />
+      <div data-testid="page-login">
+        {isLoading && <Loading />}
+        <input
+          type="text"
+          data-testid="login-name-input"
+          id="name"
+          name="name"
+          onChange={ this.handleChange }
+        />
+        <button
+          type="button"
+          data-testid="login-submit-button"
+          disabled={ isDisabled }
+          onClick={ () => this.handleClick(name) }
+        >
+          Entrar
+
+        </button>
+      </div>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+}.isRequired;
 
 export default Login;
