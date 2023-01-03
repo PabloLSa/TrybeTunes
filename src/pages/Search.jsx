5c8268ed3d726bff07239be82822a49563839cbc
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2023-01-02 23:06:31 -0300

    0.5 criando formulario para pesquisar artista

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,41 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  state = {
+    isDisabled: true,
+    name: '',
+  };
+
+  handleChange = ({ target }) => {
+    const { value } = target;
+    const num = 2;
+    const valid = value.length >= num;
+    this.setState({
+      name: value,
+      isDisabled: !valid,
+    });
+  };
+
   render() {
+    const { name, isDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <input
+          type="text"
+          data-testid="search-artist-input"
+          name="name"
+          value={ name }
+          onChange={ this.handleChange }
+        />
+        <button
+          type="button"
+          data-testid="search-artist-button"
+          disabled={ isDisabled }
+        >
+          Pesquisar
+
+        </button>
       </div>
     );
   }
