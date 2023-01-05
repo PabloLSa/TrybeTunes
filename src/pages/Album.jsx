2f3e34c1f7489fef02eee020dcbc8aa82193f84a
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2023-01-04 21:34:39 -0300

    0.7 Crianddo a lista de músicas do álbum selecionado

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,14 +1,45 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import MusicCard from '../components/ MusicCard';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
 
 class Album extends Component {
+  state = {
+    dadosArtista: [],
+    musicas: [],
+  };
+
+  async componentDidMount() {
+    const { history: { location: { pathname } } } = this.props;
+    const number = 7;
+    const iD = pathname.slice(number);
+    const music = await getMusics(iD);
+    this.setState({
+      dadosArtista: music[0],
+      musicas: music.slice(1),
+    });
+  }
+
   render() {
+    const { dadosArtista, musicas } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
+        <p data-testid="artist-name">{dadosArtista.artistName}</p>
+        <p data-testid="album-name">{dadosArtista.collectionName}</p>
+        {musicas.map((musica) => (
+          <MusicCard key={ musica.trackId } music={ musica } />
+        )) }
       </div>
     );
   }
 }
-
+Album.propTypes = {
+  history: PropTypes.shape({
+    location: PropTypes.shape({
+      pathname: PropTypes.string,
+    }),
+  }),
+}.isRequired;
 export default Album;
